237764f83a770d993bb2d9a9161e16a018331119
Antonio De Ávila Pájaro <adeavilap1@unicartagena.edu.co>
2025-07-06 23:59:00 -0500

    feat: Antonio De Ávila Pájaro - ejercicio 3 taller 2 - Herencia Empleado/Programador

---
+++ Unidad-4-poo-Antonio-DeAvilaPajaro/src/Taller2/Ejercicios/Empleado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller2.Ejercicios;
+
+public class Empleado {
+    String nombre;
+    double salario;
+
+    public Empleado(String nombre, double salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Salario: $" + salario);
+    }
+}
---
+++ Unidad-4-poo-Antonio-DeAvilaPajaro/src/Taller2/Ejercicios/MainHerenciaEmpleado.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller2.Ejercicios;
+
+public class MainHerenciaEmpleado {
+    public static void main(String[] args) {
+        Programador p1 = new Programador("Antonio De Avila", 7800000, "Java");
+        p1.mostrarDatos();
+    }
+}
