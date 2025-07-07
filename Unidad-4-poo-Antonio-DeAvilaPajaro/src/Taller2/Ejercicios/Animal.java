52ab10bac5fc3fe0469b44a6c1d30f34fc1eeff3
Antonio De Ávila Pájaro <adeavilap1@unicartagena.edu.co>
2025-07-07 00:01:19 -0500

    feat: Antonio De Ávila Pájaro - ejercicio 4 taller 2 - Herencia Animal/Perro

---
+++ Unidad-4-poo-Antonio-DeAvilaPajaro/src/Taller2/Ejercicios/Animal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller2.Ejercicios;
+
+public class Animal {
+    String nombre;
+    int edad;
+
+    public Animal(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void hacerSonido() {
+        System.out.println(nombre + " hace un sonido.");
+    }
+}
+
---
+++ Unidad-4-poo-Antonio-DeAvilaPajaro/src/Taller2/Ejercicios/MainHerenciaAnimal.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller2.Ejercicios;
+
+public class MainHerenciaAnimal {
+    public static void main(String[] args) {
+        Perro miPerro = new Perro("Firulais", 5, "Labrador");
+        miPerro.hacerSonido();
+    }
+}
