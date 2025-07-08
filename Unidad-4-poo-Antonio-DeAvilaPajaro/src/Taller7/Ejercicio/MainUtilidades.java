ea6c1512a9d25859c654d9e5a65804ab5ad7d186
Antonio De Ávila Pájaro <adeavilap1@unicartagena.edu.co>
2025-07-08 12:13:20 -0500

    feat: ejercicio 3 del Taller 7 sobre modificador public - clase Utilidades

---
+++ Unidad-4-poo-Antonio-DeAvilaPajaro/src/Taller7/Ejercicio/MainUtilidades.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller7.Ejercicio;
+
+public class MainUtilidades {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+
+        System.out.println("Suma: " + Utilidades.sumar(a, b));
+        System.out.println("Resta: " + Utilidades.restar(a, b));
+        System.out.println("Multiplicación: " + Utilidades.multiplicar(a, b));
+        System.out.println("División: " + Utilidades.dividir(a, b));
+    }
+}
+
