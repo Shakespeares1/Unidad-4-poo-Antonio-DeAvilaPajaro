e7adc20255d229f7428c2cda692023a17b54ddae
Antonio De Ávila Pájaro <adeavilap1@unicartagena.edu.co>
2025-07-08 12:53:22 -0500

    ✅ Taller 9 - Ejercicio 1 sobre uso de super en clases Persona y Empleado

---
+++ Unidad-4-poo-Antonio-DeAvilaPajaro/src/Taller9/Ejercicio/MainPersonaEmpleado.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller9.Ejercicio;
+
+public class MainPersonaEmpleado {
+    public static void main(String[] args) {
+        Empleado emp = new Empleado("Antonio", 28, "Ingeniería de Software");
+        emp.mostrarDetalles();
+    }
+}
+
