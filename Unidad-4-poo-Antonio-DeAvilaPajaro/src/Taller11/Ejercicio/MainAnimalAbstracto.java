9f0a69b7cd31c60fcc35364267258b86f6e68ac6
Antonio De Ávila Pájaro <adeavilap1@unicartagena.edu.co>
2025-07-08 19:37:21 -0500

    Taller 11 - Ejercicio 3: Uso incorrecto de clases abstractas

---
+++ Unidad-4-poo-Antonio-DeAvilaPajaro/src/Taller11/Ejercicio/MainAnimalAbstracto.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller11.Ejercicio;
+
+public class MainAnimalAbstracto {
+    public static void main(String[] args) {
+        AnimalAbstracto perro = new PerroAbsurdo();
+        perro.hacerSonido(); // Salida: El animal hace un sonido genérico.
+    }
+}
---
+++ Unidad-4-poo-Antonio-DeAvilaPajaro/src/Taller11/Ejercicio/MainInstanciacionIncorrecta.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller11.Ejercicio;
+
+public class MainInstanciacionIncorrecta {
+    public static void main(String[] args) {
+        // Error: No se puede instanciar una clase abstracta
+        // Figura figura = new Figura(); // Esto causará un error de compilación
+    }
+}
